7b97f2480b2fe28c994830fd8f5e5e4fdfa76a5f
jacky5288 <790867448@qq.com>
2017-08-07 23:42:44 +0800

    完成头部功能

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -14,12 +14,22 @@
                     {{seller.description}}/{{seller.deliveryTime}}分钟送达
                 </div>
                 <div v-if="seller.supports" class="supports">
-                    <span class="icon"></span>
+                    <span class="icon" :class="classMap[seller.supports[0].type]"></span>
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
             </div>
+            <div v-if="seller.supports" class="support-count">
+                <span class="count">{{seller.supports.length}}个</span>
+                <i class="icon-keyboard_arrow_right"></i>
+            </div>
         </div>
-        <div class="bulletin-wrapper"></div>
+        <div class="bulletin-wrapper">
+            <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+            <i class="icon-keyboard_arrow_right"></i>
+        </div>
+        <div class="background">
+            <img :src="seller.avatar" height="100%" width="100%">
+        </div>
     </div>
 
 </template>
@@ -29,11 +39,14 @@
     export  default {
 
         props: {
-
             seller: {
                 type: Object
             }
+        },
+        created(){
+            this.classMap = ['decrease','discount','special','invoice','guarantee']
         }
+
     };
 
 </script>
@@ -42,16 +55,20 @@
     @import "../../common/stylus/mixin.styl" /* 注意，这里不能使用 像引入js那样，直接common打头，需要使用相对路径*/
     .header
         color: #fff
-        background-color: #000
+        position relative
+        background-color rgba(7,17,27,0.5)
         .content-wrapper
+            position relative
             padding: 24px 12px 18px 24px
             font-size: 0
             .avatar
                 display: inline-block
+                vertical-align top
+                img
+                    border-radius 2px
             .content
                 display: inline-block
                 margin-left: 16px
-                font-size: 14px
                 .title
                     margin: 2px 0 8px 0
                     .brand
@@ -67,4 +84,91 @@
                         font-size 16px
                         line-height  18px
                         font-weight bold
+                .description
+                    margin-bottom 10px
+                    line-height 12px
+                    font-size 12px
+                .supports
+                    .icon
+                        display inline-block
+                        vertical-align top
+                        width 12px
+                        height 12px
+                        margin-right 4px
+                        background-size 12px 12px
+                        background-repeat no-repeat
+                        &.decrease
+                            bg-image("decrease_1")
+                        &.discount
+                            bg-image("discount_1")
+                        &.guarantee
+                            bg-image("guarantee_1")
+                        &.invoice
+                            bg-image("invoice_1")
+                        &.special
+                            bg-image("special_1")
+
+                    .text
+                        font-size 12px
+                        line-height 10px
+
+
+            .support-count
+                position absolute
+                right 12px
+                bottom 14px
+                padding 0 8px
+                height 24px
+                line-height 24px
+                border-radius 14px
+                background-color rgba(0, 0, 0, 0.2)
+                text-align center
+                .count
+                    font-size 10px
+                    vertical-align top
+                .icon-keyboard_arrow_right
+                    font-size 10px
+                    margin-left 2px
+                    line-height 24px
+                    height 24px
+
+
+        .bulletin-wrapper
+            height 28px
+            line-height 28px
+            padding 0 22px 0 12px
+            white-space nowrap
+            overflow hidden
+            text-overflow ellipsis
+            position relative
+            background-color rgba(7,17,27,.2)
+            .bulletin-title
+                display inline-block
+                vertical-align top
+                height 12px
+                width 22px
+                bg-image('bulletin')
+                background-repeat no-repeat
+                background-size 22px 12px
+                margin-top 7px
+            .bulletin-text
+                font-size 10px
+                margin 0 4px
+                vertical-align top
+            .icon-keyboard_arrow_right
+                position absolute
+                font-size 10px
+                right 12px
+                top 8px
+
+
+        .background
+            position absolute
+            top 0
+            left 0
+            width 100%
+            height 100%
+            z-index -1
+            filter blur(10px)
+
 </style>
