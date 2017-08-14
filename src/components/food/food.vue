cb092a5b3ded2b6684dd4851bfb39a467537d66a
jacky5288 <790867448@qq.com>
2017-08-15 00:37:22 +0800

    完成商品列表，购物车等

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,5 +1,5 @@
-<template>
-    <div class="food" v-show="showFlag" transition="move">
+<template xmlns:v-el="http://www.w3.org/1999/xhtml">
+    <div class="food" v-show="showFlag" transition="move" v-el:food>
         <div class="food-content">
             <div class="image-header">
                 <img :src="food.image">
@@ -14,33 +14,94 @@
                     <span class="rating">好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                    <span class="now">￥{{food.price}}</span>
-                    <span class="old" v-show="food.oldPrice">{{food.oldPrice}}</span>
+                    <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
+                <div class="cartcontrol-wrapper">
+                    <cartcontrol :food="food"></cartcontrol>
+                </div>
+                <div class="buy" @click.stop.prevent="addFirst" v-show="!food.count || food.count===0" transition="fade">加入购物车</div>
+            </div>
+            <split v-show="food.info"></split>
+            <div class="info" v-show="food.info">
+                <h1 class="title">商品介绍</h1>
+                <p class="text">{{food.info}}</p>
+            </div>
+            <split></split>
+            <div class="rating">
+                <h1 class="title">商品评价</h1>
+                <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
             </div>
         </div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+
+    import BScroll from 'better-scroll';
+    import Vue from 'vue';
+    import cartcontrol from 'components/cartcontrol/cartcontrol.vue';
+    import split from 'components/split/split';
+    import ratingselect from 'components/ratingselect/ratingselect';
+
+    const POSITIVE = 0;
+    const NEGATIVE = 1;
+    const ALL = 2;
+
+
     export default{
         props: {
             food: {
                 type: Object
             }
         },
+        components:{
+            cartcontrol,
+            split,
+            ratingselect
+        },
         data(){
             return {
-                showFlag: false
+                showFlag: false,
+                selectType: ALL, // 默认选择全部
+                onlyContent: true, //默认查看有内容的评论
+                desc:{
+                    all:"全部",
+                    positive:"推荐",
+                    negative:"吐槽"
+                }
             }
         },
         methods: {
+            addFirst(event){ // 不传参，默认的参数就是 $event
+                if(!event._constructed){
+                    return;
+                }
+
+                this.$dispatch('cart.add',event.target); // 点击的时候派发一个事件（传递的参数为event.target），将dom对象传递出来，实现小球飞翔动画
+                Vue.set(this.food,'count',1); // 第一次，通过vue.set来设置count属性
+            },
             show(){
                 this.showFlag = true;
+
+                this.selectType = ALL; // 默认选择全部
+                this.onlyContent = true;
+
+                this.$nextTick(()=>{
+                    if(!this.scroll){
+                        this.scroll = new BScroll(this.$els.food,{
+                            click:true
+                        });
+                    }else{
+                        this.scroll.refresh();
+                    }
+                });
             },
             hide(){
                 this.showFlag = false;
             }
+        },
+        events:{
+
         }
     }
 </script>
@@ -80,4 +141,79 @@
                     padding 10px /*小技巧，通过padding加大可点击区域*/
                     font-size 20px
                     color #fff
+        .content
+            padding 18px
+            position relative
+            .title
+                line-height 14px
+                margin-bottom 8px
+                font-size 14px
+                font-weight 700
+                color rgb(7, 17, 27)
+            .detail
+                margin-bottom 18px
+                line-height 10px
+                height 10px
+                font-size 0px
+                .sell-count, .rating
+                    font-size 10px
+                    color rgb(147, 153, 159)
+                .sell-count
+                    margin-right  12px
+            .price
+                font-weight 700
+                line-height 24px
+                .now
+                    margin-right 8px
+                    font-size 14px
+                    color rgb(240, 20, 20)
+                .old
+                    text-decoration line-through
+                    font-size 10px
+                    color rgb(147, 153, 159)
+            .cartcontrol-wrapper
+                position absolute
+                right 12px
+                bottom 12px
+            .buy
+                position absolute
+                right 18px
+                bottom 18px
+                z-index 10
+                height 24px
+                line-height 24px
+                padding 0 12px
+                box-sizing border-box
+                font-size 10px
+                border-radius 12px
+                color #fff
+                background rgb(0,160,220)
+                &.fade-transition
+                    transition all 0.2s
+                    opacity 1
+                &.fade-enter, &.fade-leave
+                    opacity 0
+
+        .info
+            padding 18px
+            .title
+                line-height 14px
+                margin-bottom 6px
+                font-size 14px
+                color rgb(7,17,27)
+            .text
+                line-height 24px
+                padding 0 8px
+                font-size 12px
+                color rgb(77,85,93)
+
+        .rating
+            padding-top 18px
+            .title
+                line-height 14px
+                margin-left 18px
+                font-size 14px
+                color rgb(7,17,27)
+
+
 </style>
